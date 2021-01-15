6c3923abf50562dcb2478147ad238236c83a2f03
Josh Frank <eurosewusa@gmail.com>
2021-01-15 09:14:43 -0500

    curly fries

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, numericality: { greater_than: 1000000000, less_than: 9999999999 }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: [ "Fiction", "Non-Fiction" ] }
+    validate :clickbait
+
+    private
+
+    def clickbait
+        if self.title && !self.title.downcase.match?( /won't believe|secret|top \d|guess/ )
+            self.errors.add( :name, "Must have clickbait in title" )
+        end
+    end
+
 end
